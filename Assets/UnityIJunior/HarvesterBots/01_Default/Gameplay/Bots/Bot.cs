0bda730341d8a5bdb40a600ea577c95e4758a6a3
Vlad Parastaev <streeetmagic@gmail.com>
2024-01-03 10:31:36 +0400

    Refactored

--- Assets/UnityIJunior/HarvesterBots/01_Default/Gameplay/Bots/Bot.cs
+++ Assets/UnityIJunior/HarvesterBots/01_Default/Gameplay/Bots/Bot.cs
@@ -1,35 +1,21 @@
-﻿using System;
-using System.Collections;
-using _01_Default.Gameplay.Bases;
+﻿using _01_Default.Gameplay.Bases;
 using _01_Default.Gameplay.Resourcess;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace _01_Default.Gameplay.Bots
 {
     public class Bot : MonoBehaviour
     {
-        [SerializeField] private float _moveSpeed = 10f;
-        [SerializeField] private CharacterController _controller;
-        [SerializeField] private Transform _spawnPosition;
-
+        [SerializeField] private BotMover _botMover;
         private Resource _target;
-        private Coroutine _movingCoroutine;
 
         public bool IsBusy { get; private set; }
-
-        private void Start()
-        {
-            _spawnPosition.transform.parent = null;
-        }
 
         public void SetTarget(Resource target)
         {
             _target = target;
-
             IsBusy = true;
-
-            Move(_target.transform);
+            _botMover.Move(_target.transform);
         }
 
         private void OnTriggerEnter(Collider other)
@@ -45,7 +31,7 @@ namespace _01_Default.Gameplay.Bots
                     Vector3 transformPosition = myTransform.position;
                     targetTransform.position = new Vector3(transformPosition.x, 2, transformPosition.z);
                     _target.GetComponent<Rigidbody>().useGravity = false;
-                    Move(_spawnPosition);
+                    _botMover.Move();
                 }
             }
 
@@ -59,48 +45,5 @@ namespace _01_Default.Gameplay.Bots
                 }
             }
         }
-
-        private void Move(Transform position)
-        {
-            if (_movingCoroutine != null)
-                StopCoroutine(_movingCoroutine);
-
-            _movingCoroutine = StartCoroutine(Moving(position));
-        }
-
-        private IEnumerator Moving(Transform targetTransform)
-        {
-            float tolerance = 0.1f;
-
-            while (IsAway(targetTransform.position, tolerance))
-            {
-                Vector3 position = targetTransform.position;
-
-                Vector3 targetTransformPosition = new(position.x, 0, position.z);
-
-                _controller.Move((targetTransformPosition - transform.position).normalized * (Time.deltaTime * _moveSpeed));
-
-                Transform transform1 = _controller.transform;
-                Vector3 position1 = transform1.position;
-
-                position1 = new Vector3(position1.x, 0, position1.z);
-                transform1.position = position1;
-
-                RotateToPosition(targetTransformPosition);
-
-                yield return null;
-            }
-
-            StopCoroutine(_movingCoroutine);
-
-            _movingCoroutine = null;
-        }
-
-        private void RotateToPosition(Vector3 targetTransformPosition) =>
-            transform.LookAt(new Vector3(targetTransformPosition.x, 0, targetTransformPosition.z));
-
-        private bool IsAway(Vector3 position, float tolerance) =>
-            Math.Abs(transform.position.x - position.x) > tolerance
-            || Math.Abs(transform.position.z - position.z) > tolerance;
     }
 }
---
+++ Assets/UnityIJunior/HarvesterBots/01_Default/Gameplay/Bots/BotMover.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace _01_Default.Gameplay.Bots
+{
+    internal class BotMover : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed = 10f;
+        [SerializeField] private CharacterController _controller;
+        [SerializeField] private Transform _spawnPosition;
+
+        private Coroutine _movingCoroutine;
+
+        private void Start()
+        {
+            _spawnPosition.transform.parent = null;
+        }
+        
+        public void Move()
+        {
+            Move(_spawnPosition);
+        }
+
+        public void Move(Transform position)
+        {
+            if (_movingCoroutine != null)
+                StopCoroutine(_movingCoroutine);
+
+            _movingCoroutine = StartCoroutine(Moving(position));
+        }
+
+        private IEnumerator Moving(Transform targetTransform)
+        {
+            float tolerance = 0.1f;
+
+            while (IsAway(targetTransform.position, tolerance))
+            {
+                Vector3 position = targetTransform.position;
+
+                Vector3 targetTransformPosition = new(position.x, 0, position.z);
+
+                _controller.Move((targetTransformPosition - transform.position).normalized * (Time.deltaTime * _moveSpeed));
+
+                Transform transform1 = _controller.transform;
+                Vector3 position1 = transform1.position;
+
+                position1 = new Vector3(position1.x, 0, position1.z);
+                transform1.position = position1;
+
+                RotateToPosition(targetTransformPosition);
+
+                yield return null;
+            }
+
+            StopCoroutine(_movingCoroutine);
+
+            _movingCoroutine = null;
+        }
+
+        private void RotateToPosition(Vector3 targetTransformPosition) =>
+            transform.LookAt(new Vector3(targetTransformPosition.x, 0, targetTransformPosition.z));
+
+        private bool IsAway(Vector3 position, float tolerance) =>
+            Math.Abs(transform.position.x - position.x) > tolerance
+            || Math.Abs(transform.position.z - position.z) > tolerance;
+    }
+}
